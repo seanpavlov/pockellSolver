4561b60a44ad2fcf427f2bafe6e392db166bbaa4
twister <simonwalmgren@gmail.com>
2014-12-16 13:55:30 +0100

    add comments and correct characters per line

--- CubePar.hs
+++ CubePar.hs
@@ -7,33 +7,98 @@ import Data.Maybe
 import Data.Char
 import System.Random
 
-
+-----------DATATYPES---------------------------------------------------------
+--A Cube is composed of a list of corners.
 data Cube = Cube { corners :: [Corner] }
 	deriving (Show, Eq)
 
-data Corner = Corner { corner :: [(Color, Face)] }
+--A corner of a cube is represented by a list of colors 
+--and their respictive faces.
+data Corner = Corner { faceColors :: [(Color, Face)] }
 	deriving (Show,Eq)
 
-
---Color corresponding to side: White(D), Yellow(U), Red(F), Orange(B), Green(R), Blue(L)
+--Color corresponding to a face on a corner.
 data Color = White | Yellow | Red | Orange | Blue | Green
 	deriving (Eq, Show, Ord)
 
+--A move is a way to rotate the cube.
 data Move = F | Fi | F2 | R | Ri | R2 | U | Ui | U2
 	deriving (Show,Eq)
 
+--Different sides of the cube.
 data Face = Ff | Bf | Lf | Rf | Uf | Df
 	deriving(Show,Eq)
 
+-----------FUNCTIONS---------------------------------------------------------
+--Starts the program.
+main :: IO ()
+main = do 
+		putStr "Input Cube: (s for shuffled cube, or colors W,Y,"
+		putStrLn "B,G,R,O in order Front,Back,Left,Right,Up,Down"
+		cs <- getLine 
+		case cs of
+			"s" -> do 
+					putStrLn "Number of rotations for shuffle:"
+					snrLine <- getLine
+					let snr = read snrLine :: Integer
+					g <- newStdGen
+					putStr "Shuffle moves: "
+					print (snd (shuffle g newSolvedCube snr))
+					putStrLn "Solving..."
+					print (snd (fromJust (solve (fst 
+						(shuffle g newSolvedCube snr)))))
+					putStrLn "Solution found! Moves: ^"
+			_ -> do
+					putStrLn "Solving..."
+					print (snd (fromJust ((solve (charsToCube(charsToColor 
+						(map toUpper cs)))))))
+					putStrLn "Solution found! Moves: ^"
+
+	where
+		--Given	list of chars translates them into colors.
+		charsToColor :: [Char] -> [Color]
+		charsToColor [] = []
+		charsToColor (c:cs)
+			| c == ' ' = charsToColor cs
+			| c == 'W' = White:charsToColor cs
+			| c == 'Y' = Yellow:charsToColor cs
+			| c == 'B' = Blue:charsToColor cs
+			| c == 'G' = Green:charsToColor cs
+			| c == 'R' = Red:charsToColor cs
+			| c == 'O' = Orange:charsToColor cs
+			| otherwise = error "Wrong input. Valid inputs are: W,Y,B,G,R,O."
+
+		--Given a list of colors, translates them into a cube.
+		charsToCube :: [Color] -> Cube
+		charsToCube cList = do
+			case (length cList) of
+				24 -> do
+					let c = Cube [Corner [((cList!!(fst x)),(snd x)) | 
+							x <- zip (fst y) (snd y)] | 
+							y <- zip cornersFromSides faces]
+					case (isValidCube c) of
+						True 	-> c
+						False 	-> error "Not a valid cube."
+				_ -> error "Incorrect number of colors."
+
+--Gives a new solved cube with: White(Down), Yellow(Up), Red(Front), 
+--Orange(Back), Green(Right), Blue(Left)
 newSolvedCube :: Cube
-newSolvedCube = Cube [Corner [(Red,Ff),(Blue,Lf),(Yellow,Uf)], Corner [(Red,Ff),(Green,Rf),(Yellow,Uf)], Corner [(Red,Ff),(Blue,Lf),(White,Df)],
-						Corner [(Red,Ff),(Green,Rf),(White,Df)], Corner [(Orange,Bf),(Blue,Lf),(Yellow,Uf)], Corner [(Orange,Bf),(Green,Rf),(Yellow,Uf)]
-						, Corner [(Orange,Bf),(Blue,Lf),(White,Df)], Corner [(Orange,Bf),(Green,Rf),(White,Df)]]
+newSolvedCube = Cube [Corner [(Red,Ff),(Blue,Lf),(Yellow,Uf)], 
+					Corner [(Red,Ff),(Green,Rf),(Yellow,Uf)], 
+					Corner [(Red,Ff),(Blue,Lf),(White,Df)],
+					Corner [(Red,Ff),(Green,Rf),(White,Df)], 
+					Corner [(Orange,Bf),(Blue,Lf),(Yellow,Uf)], 
+					Corner [(Orange,Bf),(Green,Rf),(Yellow,Uf)],
+					Corner [(Orange,Bf),(Blue,Lf),(White,Df)], 
+					Corner [(Orange,Bf),(Green,Rf),(White,Df)]]
 
 --Side order: Front(F), Back(B), Left(L), Right(R), Up(U), Down(D)
 --Block order: Upper-Left, Upper-Right, Bottom Left, Bottom-Right
 sides :: Cube -> [[Color]]
-sides c = [[extractColor (corner ((corners c)!!x)) (fst idFace) | x <- (snd idFace)] | idFace <- zip [Ff,Bf,Lf,Rf,Uf,Df] [[0..3], [5,4,7,6], [4,0,6,2], [1,5,3,7], [4,5,0,1], [2,3,6,7]]]
+sides c = [[extractColor (faceColors ((corners c)!!x)) (fst idFace) | 
+			x <- (snd idFace)] | idFace <- zip [Ff,Bf,Lf,Rf,Uf,Df] [[0..3], 
+			[5,4,7,6], [4,0,6,2], [1,5,3,7], [4,5,0,1], [2,3,6,7]]]
 	where 
 		extractColor :: [(Color,Face)] -> Face -> Color
 		extractColor [] _ = error "toooooo much painz"
@@ -41,24 +106,36 @@ sides c = [[extractColor (corner ((corners c)!!x)) (fst idFace) | x <- (snd idFa
 			| (snd c) == f 	= fst c
 			| otherwise 	= extractColor cs f
 
-
+--Rotates a cube depending on what move is given.
 rotate :: Cube -> Move -> Cube
-rotate c F 	= update c [0..3] [1,3,0,2] [[(Uf,Rf),(Lf,Uf)],[(Rf,Df),(Uf,Rf)],[(Lf,Uf),(Df,Lf)],[(Df,Lf),(Rf,Df)]]
-rotate c Fi = update c [0..3] [2,0,3,1] [[(Lf,Df),(Uf,Lf)],[(Uf,Lf),(Rf,Uf)],[(Df,Rf),(Lf,Df)],[(Rf,Uf),(Df,Rf)]]
-rotate c F2 = update c [0..3] [3,2,1,0] [[(Lf,Rf),(Uf,Df)],[(Uf,Df),(Rf,Lf)],[(Df,Uf),(Lf,Rf)],[(Rf,Lf),(Df,Uf)]]
-rotate c U 	= update c [0,1,4,5] [4,0,5,1] [[(Lf,Bf),(Ff,Lf)],[(Ff,Lf),(Rf,Ff)],[(Bf,Rf),(Lf,Bf)],[(Rf,Ff),(Bf,Rf)]]
-rotate c Ui = update c [0,1,4,5] [1,5,0,4] [[(Ff,Rf),(Lf,Ff)],[(Rf,Bf),(Ff,Rf)],[(Lf,Ff),(Bf,Lf)],[(Bf,Lf),(Rf,Bf)]]
-rotate c U2 = update c [0,1,4,5] [5,4,1,0] [[(Ff,Bf),(Lf,Rf)],[(Rf,Lf),(Ff,Bf)],[(Lf,Rf),(Bf,Ff)],[(Bf,Ff),(Rf,Lf)]]
-rotate c R 	= update c [1,3,5,7] [5,1,7,3] [[(Uf,Bf),(Ff,Uf)],[(Ff,Uf),(Df,Ff)],[(Bf,Df),(Uf,Bf)],[(Df,Ff),(Bf,Df)]]
-rotate c Ri = update c [1,3,5,7] [3,7,1,5] [[(Ff,Df),(Uf,Ff)],[(Df,Bf),(Ff,Df)],[(Uf,Ff),(Bf,Uf)],[(Bf,Uf),(Df,Bf)]]
-rotate c R2 = update c [1,3,5,7] [7,5,3,1] [[(Ff,Bf),(Uf,Df)],[(Df,Uf),(Ff,Bf)],[(Uf,Df),(Bf,Ff)],[(Bf,Ff),(Df,Uf)]]
-
-update :: Cube -> [Int] -> [Int] -> [[(Face,Face)]] -> Cube
-update c oldPos newPos fs = updateCorners c (updateFaces [((corners c)!!x)|x <- oldPos] fs) newPos
-
-updateFaces :: [Corner] -> [[(Face,Face)]] -> [Corner] 
-updateFaces c fs = [Corner (updateFace (corner (fst x)) (snd x)) | x <- (zip c fs)]
+rotate c f
+	| f == F 	= update c [0..3] [1,3,0,2] [[(Uf,Rf),(Lf,Uf)],
+					[(Rf,Df),(Uf,Rf)],[(Lf,Uf),(Df,Lf)],[(Df,Lf),(Rf,Df)]]
+	| f == Fi 	= update c [0..3] [2,0,3,1] [[(Lf,Df),(Uf,Lf)],
+					[(Uf,Lf),(Rf,Uf)],[(Df,Rf),(Lf,Df)],[(Rf,Uf),(Df,Rf)]]
+	| f == F2 	= update c [0..3] [3,2,1,0] [[(Lf,Rf),(Uf,Df)],
+					[(Uf,Df),(Rf,Lf)],[(Df,Uf),(Lf,Rf)],[(Rf,Lf),(Df,Uf)]]
+	| f == U 	= update c [0,1,4,5] [4,0,5,1] [[(Lf,Bf),(Ff,Lf)],
+					[(Ff,Lf),(Rf,Ff)],[(Bf,Rf),(Lf,Bf)],[(Rf,Ff),(Bf,Rf)]]
+	| f == Ui 	= update c [0,1,4,5] [1,5,0,4] [[(Ff,Rf),(Lf,Ff)],
+					[(Rf,Bf),(Ff,Rf)],[(Lf,Ff),(Bf,Lf)],[(Bf,Lf),(Rf,Bf)]]
+	| f == U2 	= update c [0,1,4,5] [5,4,1,0] [[(Ff,Bf),(Lf,Rf)],
+					[(Rf,Lf),(Ff,Bf)],[(Lf,Rf),(Bf,Ff)],[(Bf,Ff),(Rf,Lf)]]
+	| f == R 	= update c [1,3,5,7] [5,1,7,3] [[(Uf,Bf),(Ff,Uf)],
+					[(Ff,Uf),(Df,Ff)],[(Bf,Df),(Uf,Bf)],[(Df,Ff),(Bf,Df)]]
+	| f == Ri 	= update c [1,3,5,7] [3,7,1,5] [[(Ff,Df),(Uf,Ff)],
+					[(Df,Bf),(Ff,Df)],[(Uf,Ff),(Bf,Uf)],[(Bf,Uf),(Df,Bf)]]
+	| f == R2 	= update c [1,3,5,7] [7,5,3,1] [[(Ff,Bf),(Uf,Df)],
+					[(Df,Uf),(Ff,Bf)],[(Uf,Df),(Bf,Ff)],[(Bf,Ff),(Df,Uf)]]
 	where
+		update :: Cube -> [Int] -> [Int] -> [[(Face,Face)]] -> Cube
+		update c oldPos newPos fs = updateCorners c (updateFaces 
+			[((corners c)!!x)|x <- oldPos] fs) newPos
+			
+		updateFaces :: [Corner] -> [[(Face,Face)]] -> [Corner] 
+		updateFaces c fs = [Corner (updateFace 
+			(faceColors (fst x)) (snd x)) | x <- (zip c fs)]
+			
 		updateFace :: [(Color,Face)] -> [(Face,Face)] -> [(Color,Face)]
 		updateFace c [] = c
 		updateFace c (ff:ffs) = updateFace (updateSide c ff) ffs
@@ -68,25 +145,26 @@ updateFaces c fs = [Corner (updateFace (corner (fst x)) (snd x)) | x <- (zip c f
 			| not ((fst ff) == (snd c)) = c:(updateSide cs ff)
 			| otherwise					= ((fst c),(snd ff)):cs   
 
+		updateCorners :: Cube -> [Corner] -> [Int] -> Cube
+		updateCorners  c [] [] = c
+		updateCorners  cube (c:cs) (p:ps) = 
+			updateCorners (Cube ((corners cube)!!=(p,c))) cs ps
 
-updateCorners :: Cube -> [Corner] -> [Int] -> Cube
-updateCorners  c [] [] = c
-updateCorners  cube (c:cs) (p:ps) = updateCorners (Cube ((corners cube)!!=(p,c))) cs ps
-
-(!!=) :: [a] -> (Int,a) -> [a]
-(!!=) [] 		_ 	= []
-(!!=) (x:xs) (0,a) 	= a:xs
-(!!=) (x:xs) (i,a)
-	| i < 0 	= error "Invalid index" 
-	| otherwise = x:(xs !!= (i-1,a))
+		(!!=) :: [a] -> (Int,a) -> [a]
+		(!!=) [] 		_ 	= []
+		(!!=) (x:xs) (0,a) 	= a:xs
+		(!!=) (x:xs) (i,a)
+			| i < 0 	= error "Invalid index" 
+			| otherwise = x:(xs !!= (i-1,a))
 
 
 --Check if the cube is solved by checking that each side only has one color
 isSolved :: Cube -> Bool
 isSolved c = all (==1) [length (groupBy (==) side) | side <- (sides c)]
 
---Takes a StdGen, cube and number of random moves it should do and shuffles it
---Returns a tuple with the shuffled cube and a list of moves it made to shuffle
+--Takes a StdGen, cube and number of random moves it should do
+--and shuffles it. Returns a tuple with the shuffled cube
+--and a list of moves it made to shuffle
 shuffle :: StdGen -> Cube -> Integer -> (Cube, [Move])
 shuffle g c i = shuffle' g c i []
 	where
@@ -108,7 +186,9 @@ shuffle g c i = shuffle' g c i []
 				rotateSide c 6 = ((rotate c R), R)
 				rotateSide c 7 = ((rotate c Ri), Ri)
 				rotateSide c 8 = ((rotate c R2), R2)
-				
+
+--Rotates a cube all possible ways in all states recursievly
+--and returns the first occurence of a solved cube.			
 solve :: Cube -> Maybe (Cube,[Move])
 solve c = solve'' (c,[]) moveList
 	where
@@ -123,68 +203,29 @@ solve c = solve'' (c,[]) moveList
 		solve'' :: (Cube,[Move]) -> [Move] -> Maybe (Cube,[Move])
 		solve'' cm (m:[]) 	= solve' ((rotate (fst cm) m),(snd cm)++[m]) m 
 		solve'' cm (m:ms) 	= do
-								let k = solve' ((rotate (fst cm) m),(snd cm)++[m]) m 
-								case k of
-									Nothing -> solve'' cm ms
-									Just _ 	-> k
-
+			let k = solve' ((rotate (fst cm) m),(snd cm)++[m]) m 
+			case k of
+				Nothing -> solve'' cm ms
+				Just _ 	-> k
 
 sortCM (c1,m1) (c2,m2)
 	| (length m1) <= (length m2) 	= GT
 	| (length m1) > (length m2) 	= LT
 
-main :: IO ()
-main = do 
-		putStrLn "Input Cube:"
-		cs <- getLine 
-		case cs of
-			"s" -> do 
-					putStrLn "Number of rotations for shuffle:"
-					snrLine <- getLine
-					let snr = read snrLine :: Integer
-					g <- newStdGen
-					putStr "Shuffle moves: "
-					print (snd (shuffle g newSolvedCube snr))
-					putStrLn "Solving..."
-					print (snd (fromJust (solve (fst (shuffle g newSolvedCube snr)))))
-					putStrLn "Solution found! Moves: ^"
-			_ -> do
-					putStrLn "Solving..."
-					print (snd (fromJust ((solve (charsToCube(charsToColor (map toUpper cs)))))))
-					putStrLn "Solution found! Moves: ^"
-		
-charsToColor :: [Char] -> [Color]
-charsToColor [] = []
-charsToColor (c:cs)
-	| c == ' ' = charsToColor cs
-	| c == 'W' = White:charsToColor cs
-	| c == 'Y' = Yellow:charsToColor cs
-	| c == 'B' = Blue:charsToColor cs
-	| c == 'G' = Green:charsToColor cs
-	| c == 'R' = Red:charsToColor cs
-	| c == 'O' = Orange:charsToColor cs
-	| otherwise = error "Wrong input. Valid inputs are: W, Y, B, G, R, O."
 
-charsToCube :: [Color] -> Cube
-charsToCube cList = do
-						case (length cList) of
-							24 -> do
-								let c = Cube [Corner [((cList!!(fst x)),(snd x)) | 
-										x <- zip (fst y) (snd y)] | y <- zip cornersFromSides faces]
-								case (isValidCube c) of
-									True 	-> c
-									False 	-> error "Not a valid cube."
-							_ -> error "Incorrect number of colors."
-
+--Checks whether a cube is valid.
+--(Correct number of colors, faces on edges and number of corners)
 isValidCube :: Cube -> Bool
-isValidCube c = and [(correctColors c),(correctNbrOfCorners c),(correctFaces c)]
+isValidCube c = and [(correctColors c),
+	(correctNbrOfCorners c),(correctFaces c)]
 	where
 		correctColors :: Cube -> Bool
-		correctColors c = all (\x -> (length x)==4) (groupBy (==) (sort (foldr (++) [] (sides c))))
+		correctColors c = all (\x -> (length x)==4) 
+			(groupBy (==) (sort (foldr (++) [] (sides c))))
 		correctNbrOfCorners :: Cube -> Bool
 		correctNbrOfCorners c = ((length (corners c)) == 8)
 		correctFaces :: Cube -> Bool
-		correctFaces c = and [checkCorner ((corner (fst x)),(snd x)) | 
+		correctFaces c = and [checkCorner ((faceColors (fst x)),(snd x)) | 
 			x <- (zip (corners c) faces)]
 			where 
 				checkCorner :: ([(Color,Face)],[Face]) -> Bool
@@ -192,10 +233,12 @@ isValidCube c = and [(correctColors c),(correctNbrOfCorners c),(correctFaces c)]
 				checkCorner ((c:cs),fs) = (elem (snd c) fs) &&
 					(checkCorner (cs,(delete (snd c) fs)))
 
-cornersFromSides = [[0,9,18],[1,12,19],[2,11,20],[3,14,21],[5,8,16],[4,13,17],[7,10,22],[6,15,23]]
-faces = [[Ff,Lf,Uf],[Ff,Rf,Uf],[Ff,Lf,Df],[Ff,Rf,Df],[Bf,Lf,Uf],[Bf,Rf,Uf],[Bf,Lf,Df],[Bf,Rf,Df]]
+cornersFromSides = [[0,9,18],[1,12,19],[2,11,20],[3,14,21],
+					[5,8,16],[4,13,17],[7,10,22],[6,15,23]]
+faces = [[Ff,Lf,Uf],[Ff,Rf,Uf],[Ff,Lf,Df],[Ff,Rf,Df],
+					[Bf,Lf,Uf],[Bf,Rf,Uf],[Bf,Lf,Df],[Bf,Rf,Df]]
 
------------PROPS-----------------------------------------------------------------------------------
+-----------PROPS-------------------------------------------------------------
 
 prop_sides :: Cube -> Bool
 prop_sides c = all (\x -> (length x) == 4) (sides c)
